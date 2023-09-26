b3cdb0e708deec435da2e55ed57f9c4fd0806a6a
Matheus Leal <matheuslealazevedo@gmail.com>
2023-09-26 03:40:54 -0300

    desafio completo

--- src/index.js
+++ src/index.js
@@ -4,4 +4,23 @@ if (xp < 1000) {
     console.log("O herói de nome " + nome + " está no elo ferro");
 } else if (xp >= 1001 && xp <= 2000){
     console.log("O herói de nome " + nome + " está no elo Bronze");
+} else if (xp >= 2001 && xp <= 5000) {
+    console.log("O herói de nome " + nome + " está no elo Prata");
+} else if (xp >= 6001 && xp <= 7000) {
+    console.log("O herói de nome " + nome + " está no elo Ouro");
+} else if (xp >= 7001 && xp <= 8000) {
+    console.log("O herói de nome " + nome + " está no elo Platina");
+} else if (xp >= 8001 && xp <= 9000) {
+    console.log("O herói de nome " + nome + " está no elo Ascendente");
+} else if (xp >= 9001 && xp <= 10000) {
+    console.log("O herói de nome " + nome + " está no elo Imortal");
+} else if (xp >= 10001) {
+    console.log("O herói de nome " + nome + " está no elo Radiante");
 }
+
+
+
+
+
+
+
