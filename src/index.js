a7d04f04030e898943ffe0b999dce92ea195d453
Matheus Leal <matheuslealazevedo@gmail.com>
2023-09-26 03:36:28 -0300

    inicio do desafio

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log ("oi");
+var nome = prompt("digite o nome do personagem")
+var xp = parseInt(prompt("digite a quantidade de xp do personagem"))
+if (xp < 1000) {
+    console.log("O herói de nome " + nome + " está no elo ferro");
+} else if (xp >= 1001 && xp <= 2000){
+    console.log("O herói de nome " + nome + " está no elo Bronze");
+}
